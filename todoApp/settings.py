3ae07d10634b6e84f8d4ce276bffb9a1be10d90f
Ayeni Olakunle <ayeniolakunle98@gmail.com>
2022-07-27 13:05:53 +0100

    Update

--- todoApp/settings.py
+++ todoApp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-%b%#0r6+cu#g!7girc)3vp_%cwrrz*2u5$otc-=l-9+=%*#x4n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["python-api-student.herokuapp.com"]
+ALLOWED_HOSTS = ["python-api-student.herokuapp.com", 'localhost', '127.0.0.1']
 
 
 # Application definition
