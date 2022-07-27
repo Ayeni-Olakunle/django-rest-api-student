965a28760ac31e4182443efb336ed15a599664f0
Ayeni Olakunle <ayeniolakunle98@gmail.com>
2022-07-27 12:53:26 +0100

    Update

--- todoApp/settings.py
+++ todoApp/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 import django_on_heroku
 django_on_heroku.settings(locals())
