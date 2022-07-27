ba3c7a25174d84e21ef5f7a55dfde0d46666b61f
Ayeni Olakunle <ayeniolakunle98@gmail.com>
2022-07-27 16:24:04 +0100

    Update

--- todoApp/settings.py
+++ todoApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%b%#0r6+cu#g!7girc)3vp_%cwrrz*2u5$otc-=l-9+=%*#x4n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [student-endpoint]
 
 
 # Application definition
