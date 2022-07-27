206ebd9bebb0640178e3b40ad9a19f602158fc80
Ayeni Olakunle <ayeniolakunle98@gmail.com>
2022-07-27 13:26:29 +0100

    Update

--- todoApp/urls.py
+++ todoApp/urls.py
@@ -21,6 +21,5 @@ from learn.views import TesViewPost
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api-auth/", include('rest_framework.urls')),
-    # path("", TesView.as_view(), name="tets")
-    path("sendme", TesViewPost.as_view(), name="tets")
+    path("", TesViewPost.as_view(), name="tets2")
 ]
